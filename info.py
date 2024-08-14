3d553e6f6541228760ab2878d91647087bedac4b
abdulkhalik88822 <125821386+abdulkhalik88822@users.noreply.github.com>
2024-08-14 17:14:52 +0530

    Update info.py

--- info.py
+++ info.py
@@ -71,7 +71,7 @@ CLONE_DATABASE_URI = environ.get('CLONE_DATABASE_URI', "mongodb+srv://kailash:pa
 PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', 'bots_up') # Public Channel Username Without @ or without https://t.me/ and Bot Is Admin With Full Right.
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/backup899')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+U2sFQes7Vb8wYjk1')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/backup899')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/howtodownload088')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'backup899') # Support Chat Link Without https:// or @
