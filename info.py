38df87a0887507b6f49981f6e81a56c75f1ea1c6
abdulkhalik88822 <125821386+abdulkhalik88822@users.noreply.github.com>
2024-08-18 17:32:35 +0530

    Update info.py

--- info.py
+++ info.py
@@ -106,9 +106,9 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', 'https://t.me/howtodownload088')
 
 # Shortlink Info
-SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', False))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'api.shareus.io')
-SHORTLINK_API = environ.get('SHORTLINK_API', 'hRPS5vvZc0OGOEUQJMJzPiojoVK2')
+SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True))
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'instantlinks.co')
+SHORTLINK_API = environ.get('SHORTLINK_API', 'a8bafb8a6c3490eebbb7f71297ac49c76efe319a')
 
 # Others
 MAX_B_TN = environ.get("MAX_B_TN", "5")
